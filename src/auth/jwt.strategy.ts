4879e10f57c0cb54fa1eb94a33c49849872af4e6
gtckevin <tianchenkevin@gmail.com>
2020-07-01 23:33:34 -0400

    Add config file for default, dev, production and envirment valueable. Replace in the code as well

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,6 +5,8 @@ import {JwtPayload} from "./jwt-payload.interface";
 import {UserRepository} from "./user.repository";
 import {InjectRepository} from "@nestjs/typeorm";
 import {User} from "./user.entity";
+import * as config from 'config';
+
 
 
 @Injectable()
@@ -16,7 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'topSecret51',
+            secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
         });
     }
 
